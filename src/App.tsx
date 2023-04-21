691a57d48274366b6a02ae8ba852af1fd4261add
Piotrek197 <okroj.piotrek@gmail.com>
2023-04-21 09:26:43 +0200

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
-import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
 import ProductPage from "./components/ProductPage";
 
 function App() {
-  const [viewCart, setViewCart] = useState(false);
-
-  // const pageContent = viewCart ? <Cart /> : <ProductList />;
 
   const content = (
     <BrowserRouter>
-      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      <Header/>
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
-      {/* {pageContent} */}
-      {/* <Footer viewCart={viewCart} /> */}
     </BrowserRouter>
   );
 
